2586c451ca97a3b857a6188c548d29c9514c587c
Daniel Barros <danielbarros.android@gmail.com>
2019-05-13 23:23:22 -0300

    Segundo commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PrimeiroComponente from './componentes/PrimeiroComponente.jsx'
 
 const elemento = document.getElementById('root')
-ReactDOM.render(<h1>Olá React!</h1>, elemento)
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente />
+    </div>
+, elemento)
